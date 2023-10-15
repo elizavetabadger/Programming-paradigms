dec1b6875ecd89426f859e72965d1dad207f114f
elizavetabadger <Artbel543@yandex.ru>
2023-10-15 21:13:03 +0300

    hw1

--- homework1_task2.py
+++ homework1_task2.py
@@ -1,10 +1,9 @@
 """ Задача №2
-Написать точно такую же процедуру, но в декларативном стиле """
+Дан список целых чисел numbers. Необходимо написать в декларативном стиле процедуру для
+сортировки числа в списке в порядке убывания. Можно использовать любой алгоритм сортировки. """
 
-def sort_list_declarative(nambers):
+def sort_list_declarative(numbers):
     numbers.sort(reverse=True)
     return numbers
 
-print(f"Declarative style -> {sort_list_imperative([34, 2, -3, 16, 12, -14])}")
-
- 
+print(f"Declarative style -> {sort_list_imperative([21, 2, -3, 16, 7, 35, -14])}")
