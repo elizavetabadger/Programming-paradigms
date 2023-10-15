a1852c0229cc2f6046178f1723d36a7189285452
elizavetabadger <Artbel543@yandex.ru>
2023-10-15 21:07:19 +0300

    hw1_t2

---
+++ homework1_task2.py
@@ -0,0 +1,10 @@
+""" Задача №2
+Написать точно такую же процедуру, но в декларативном стиле """
+
+def sort_list_declarative(nambers):
+    numbers.sort(reverse=True)
+    return numbers
+
+print(f"Declarative style -> {sort_list_imperative([34, 2, -3, 16, 12, -14])}")
+
+ 
